187cee686446fc918c1a015eb8551af7088ba18f
Rodrigo Rosseti Tozatti <rodrigo.tozatti@gotmail.com>
2026-05-28 14:13:24 -0300

    Teste login

---
+++ tests/login_test.js
@@ -0,0 +1,12 @@
+/// <reference path='../steps.d.ts' />
+
+Feature('login');
+
+Scenario('Cenário 01 - APP deve realizar login com sucesso', ({ I }) => {
+    // Preeencher campo e-mail
+    I.fill    // Preencher campo senha
+    
+    // Clicar botão Entrar
+
+    // Validar login com sucesso
+});
